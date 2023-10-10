393d12234bd87451d89b7deea3621df8bab2e4a2
Stefen Wakefield <me@xstefen.dev>
2023-10-10 18:29:47 -0500

    HDPK Pantah 14 20231009

---
+++ kernel-headers/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Oct 7 01:26:34 UTC 2023"
+#define LINUX_COMPILE_BY "build-user"
+#define LINUX_COMPILE_HOST "build-host"
+#define LINUX_COMPILER "Android (8508608, based on r450784e) clang version 14.0.7 (https://android.googlesource.com/toolchain/llvm-project 4c603efb0cca074e9238af8b4106c30add4418f6), "
